8d6b987f8c834095bcce273ac4024a1d6f2c2b40
zhaoy <yan.zhao@idiaoyan.com>
2019-11-05 14:47:31 +0800

    feat: socket

--- net/socket_client.py
+++ net/socket_client.py
@@ -8,10 +8,11 @@ import socket
 
 HOST = '127.0.0.1'
 PORT = 8085
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+data = "GET / HTTP/1.1 \r\nHOST 127.0.0.1:8085\r\n"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'hello world.')
-    data = s.recv(1024)
-
+s.send(data.encode('utf-8'))
+data = s.recv(1024)
 print('Received :', repr(data))
+s.close()
